a9bd10344f74e7ee181580d2aad84d807e7f6353
github username <github email address>
2018-02-02 13:42:35 -0500

    Reverted to my solution

--- js/todo.js
+++ js/todo.js
@@ -2,23 +2,23 @@ $(document).ready(function(){
   submitForm()
 });
 
-//
-// function submitForm(){
-//   $('form').on('submit', function(event){
-//       const inputText = document.querySelector('input[type="text"]').value
-//       let l_item = document.createElement("li")
-//       l_item.innerHTML = inputText
-//       $('ol').append(l_item);
-//       event.preventDefault();
-//
-//     });
-//
-// }
 
-function submitForm() {
+function submitForm(){
   $('form').on('submit', function(event){
-    var item = $('#item').val();
-    $("ol").append($('<li>').text(item));
-    event.preventDefault();
-  });
+      const inputText = document.querySelector('input[type="text"]').value
+      let l_item = document.createElement("li")
+      l_item.innerHTML = inputText
+      $('ol').append(l_item);
+      event.preventDefault();
+
+    });
+
 }
+
+// function submitForm() {
+//   $('form').on('submit', function(event){
+//     var item = $('#item').val();
+//     $("ol").append($('<li>').text(item));
+//     event.preventDefault();
+//   });
+// }
